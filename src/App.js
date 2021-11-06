6ef5d6edb9a41c75672fec36a5a31de7550d7f7d
TshilidziN <64367951+TshilidziN@users.noreply.github.com>
2021-11-06 11:48:52 +0200

    home page css, wishLish page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import WishList from './components/WishList/WishList'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Navbar />
       </header>
       <Home />
+    
     </div>
   );
 }
---
+++ src/components/WishList/WishList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WishList() {
+    return (
+        <div>
+            <p> Hey this is the wishlist</p>
+        </div>
+    )
+}
+
+export default WishList
